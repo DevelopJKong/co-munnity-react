e5d47bf927cf64a6eaa10bff3135759bb2dbe4a1
DevelopJKong <ybaram40@gmail.com>
2021-11-12 15:42:56 +0900

    3일차 완료

--- src/Youtube.js
+++ src/Youtube.js
@@ -1,18 +1,13 @@
 //src/Hello.js
-
+import styles from "./App.module.css";
 function Youtube() {
     return <div>
-                <h1 className="color--red"><i class="fab fa-youtube"></i>유튜브</h1>
+                <h1 className={styles.colorRed}><i class="fab fa-youtube"></i>유튜브</h1>
                 <p>유튜브(영어:YouTube)는 구글이 서비스하는 동영상 공유 플랫폼이다</p>
                 <p>2006년에 구글이 인수했고, 전세계 최대 규모의 동영상 공유 및 호스팅 사이트로서, 이용자가 영상을 시청, 업로드, 공유할수있다</p>
                 <p>유튜브의 본사는 미국 캘리포니아주 샌브루노에 위치해있다</p>
            </div>;
 }
 
-function YoutubeContent() {
-    return <div>
-        <div>hello</div>
-    </div>
-}
 
 export default Youtube;
--- src/YoutubeContent.js
+++ src/YoutubeContent.js
@@ -3,7 +3,7 @@ import styles from './App.module.css';
 function YoutubeContent() {
   return (
     <div>
-      <h2 className={styles.title}>
+      <h2 className={styles.colorBlue}>
         <i class="far fa-star"></i>코뮤니티
       </h2>
       <p>열심히 코딩을 해봐요</p>
