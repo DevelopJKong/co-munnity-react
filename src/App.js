19920cca6b785dab7cd7d084ba86d146b267aae4
DevelopJKong <ybaram40@gmail.com>
2021-11-18 00:13:34 +0900

    6일차 완료

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
-import React, { useState } from 'react';
-import Button from './Button';
-import Layout from './Layout';
+// src/App.js
+import youtubeData from './data/youtubeData.json';
 
 function App() {
-  const [number, setNumber] = useState(0);
 
-  function onClickIncrease() {
-    setNumber(number + 1);
-  }
-  function onClickDecrease() {
-    setNumber(number - 1);
-  }
-
-  function onClickMultiply() {
-    setNumber(number * 2);
-  }
-  return (
-    <Layout>
-      <div>코뮤니티 구구단</div>
-      <div>{number}*1 = {number * 1}</div>
-      <div>{number}*2 = {number * 2}</div>
-      <div>{number}*3 = {number * 3}</div>
-      <div>{number}*4 = {number * 4}</div>
-      <div>{number}*5 = {number * 5}</div>
-      <div>{number}*6 = {number * 6}</div>
-      <div>{number}*7 = {number * 7}</div>
-      <div>{number}*8 = {number * 8}</div>
-      <div>{number}*9 = {number * 9}</div>
-      <Button onClick={onClickIncrease} text="+1" />
-      <Button onClick={onClickDecrease} text="-1" />
-      <Button onClick={onClickMultiply} text="2*" />
-    </Layout>
-  );
+  console.log('youtubeData : ', youtubeData);
+  console.log('youtubeData[data]', youtubeData['data']);
+  console.log('youtubeData[data][0]', youtubeData['data'][0]);
+  return <div>리액트로 데이터 불러오기</div>;
 }
 
 export default App;
